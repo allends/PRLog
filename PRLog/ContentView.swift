68ffd11323c99103a5798d4584d4a9c9bc22a785
Allen Davis-Swing <allen.davisswing@gmail.com>
2020-11-27 11:33:28 -0500

    Initial Commit

---
+++ PRLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PRLog
+//
+//  Created by Allen Davis-Swing on 11/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PRLog/PRLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRLogApp.swift
+//  PRLog
+//
+//  Created by Allen Davis-Swing on 11/27/20.
+//
+
+import SwiftUI
+
+@main
+struct PRLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
